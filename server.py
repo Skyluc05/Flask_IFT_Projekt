d188e5d01271aba260c17b856b94c2aae181b6fc
Skyluc05 <152433467+Skyluc05@users.noreply.github.com>
2025-02-28 13:02:51 +0100

    Erste Website mit der Beschriftung "KEK"

--- server.py
+++ server.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route ("/", methods = ["GET"])
+def get_index():
+    return "KEK"
 
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug = True)
