e4207d98d7775adea98ac0d298bd892b46a14fbb
JutanDragon <154429364+JutanDragon@users.noreply.github.com>
2025-03-05 09:30:11 +0100

    Templates und Homepage
    Ganz billig mit Chgpt generierte Homepage mit Buttons für Aufgabenbereiche und ganz komplex ausgelagerte Templates Dictonary

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route ("/", methods = ["GET"])
 def get_index():
-    return "KEK"
+    return render_template('homepage.html')
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
