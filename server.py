ae3dfb9f42b283951ec8071b32e79b3d50e5cfbb
JutanDragon <154429364+JutanDragon@users.noreply.github.com>
2025-03-21 11:57:29 +0100

    Homepage überarbeitet
    Neue buttons in nem Grid (Delete)

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash
+
+
+
+
+
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'  # Wichtig für Flash-Nachrichten und Sessions
@@ -7,6 +13,7 @@ USER_CREDENTIALS = {
     'username': 'admin',
     'password': '123'
 }
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -21,9 +28,11 @@ def login():
 
     return render_template("login.html")
 
+
 @app.route("/homepage")
 def homepage():
     return render_template("homepage.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
